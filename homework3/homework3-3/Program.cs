da287b71ffd04275a5898addca4fe0509e985d27
LosRR <losrr@mail.ru>
2022-12-03 23:34:17 +0300

    начали 3 домашнее задание

---
+++ homework3/homework3-3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+int GetNumber(string message)
+{
+    int result = 0
+    bool isCorrect = false;
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число, введите число большее 1")
+        }
+    }
+    return result;
+}
