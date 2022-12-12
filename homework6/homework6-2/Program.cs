c23ce46e6d6755e1d2ef0850eaa981c9987e0af1
LosRR <losrr@mail.ru>
2022-12-13 01:01:39 +0300

    готово задание 6

---
+++ homework6/homework6-2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 
+и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите число");
+        }
+    }
+    return result;
+}
+double b1 = GetNumber("Укажите число b1");
+double k1 = GetNumber("Укажите число k1");
+double b2 = GetNumber("Укажите число b2");
+double k2 = GetNumber("Укажите число k2");
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
---
+++ homework6/homework6-3/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать копию заданного 
+массива с помощью поэлементного копирования.
+*/
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+       array[i] = rnd.Next(1,9);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод, введите число");
+        }
+    }
+    return result;
+}
+int[] CopyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[count] = array[i];
+        count++;
+    }
+    return result;
+}
+int dimension = GetNumber("Введите размерность массива");
+int[] startArray = InitArray(dimension);
+Console.WriteLine("Вот наш случайный массив: ");
+PrintArray(startArray);
+int[] resultArray = CopyArray(startArray);
+Console.WriteLine("Вот копия массива: ");
+PrintArray(resultArray);
